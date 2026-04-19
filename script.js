3f9d8d9676b23adca714c22a354c4b9f2441ffce
Shriya Shrivastav <shriyashrivastav@Shriyas-MacBook-Air-399.local>
2026-04-19 12:43:40 +0530

    Fix UI hanging on neural model loading state

--- script.js
+++ script.js
@@ -332,8 +332,12 @@ const faceMesh = new FaceMesh({locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
 }});
 faceMesh.setOptions({ maxNumFaces: 2, refineLandmarks: true, minDetectionConfidence: 0.5, minTrackingConfidence: 0.5 });
+let isFaceMeshReady = false;
 faceMesh.onResults((results) => {
-    if (!startBtn.disabled && modelsLoadedCount < 2) checkAllModelsLoaded();
+    if (!isFaceMeshReady) {
+        isFaceMeshReady = true;
+        checkAllModelsLoaded();
+    }
 
     canvasCtx.save();
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
